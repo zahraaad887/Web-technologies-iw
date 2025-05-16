aec78f8b29253c8d714d56a9d8134d69fefad1fa
zahraaad227 <zehraadgozelova@gmail.com>
2025-05-16 10:12:56 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class SalamProqrami {
+public class Welcomeprogram {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
